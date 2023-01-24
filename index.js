1204ce61a85f17396616b787c5dd1428d43c1c86
Alex666777666 <111201814+Alex666777666@users.noreply.github.com>
2023-01-24 19:14:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ getItemsList()
 export const getItemsArray = text => {
   const elementsArray = document.querySelectorAll('.tool')
   console.dir(Array.from(elementsArray))
-  return elementsArray
+  return Array.from(elementsArray)
 }
 
 getItemsArray()
